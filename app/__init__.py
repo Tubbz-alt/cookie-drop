d401dec4eb99dbd163f4a56ffdb0a8cea66c3422
Jason Myers <jason@jasonamyers.com>
2013-08-18 13:37:29 -0500

    Adding home page redirect
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,9 +6,16 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+
+@app.route('/')
+def home():
+    return redirect(url_for('posts.home'))
+
 
 from app.posts.views import mod as postsModule
 app.register_blueprint(postsModule)
