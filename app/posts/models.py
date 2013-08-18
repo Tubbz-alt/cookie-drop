c0761a230eb0065dc583f77e6f579d496787dd83
Jason Myers <jason@jasonamyers.com>
2013-08-18 10:50:18 -0500

    Fixing stuffs
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- app/posts/models.py
+++ app/posts/models.py
@@ -7,7 +7,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     secret = db.Column(db.String(512))
     long = db.Column(db.Numeric(precision=8, scale=6))
-    lat = db.Column(db.Numeric(prevision=8, scale=6))
+    lat = db.Column(db.Numeric(precision=8, scale=6))
     created_time = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
 
     def __init__(self, secret, long, lat):
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from werkzeug import check_password_hash, generate_password_hash
 
