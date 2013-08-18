df83ed716a4f18046b2b04da115a13172a7f454f
Jason Myers <jason@jasonamyers.com>
2013-08-18 10:31:17 -0500

    Adding initial model
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

---
+++ app/posts/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    secret = db.Column(db.String(512))
+    long = db.Column(db.Numeric(precision=8, scale=6))
+    lat = db.Column(db.Numeric(prevision=8, scale=6))
+    created_time = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
+
+    def __init__(self, secret, long, lat):
+        self.secret = secret
+        self.long = long
+        self.lat = lat
