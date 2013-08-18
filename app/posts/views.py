e1b63b99e3ebaf64b526b99baf009169f1a84125
Brian Dailey <brian@stratasan.com>
2013-08-18 12:01:56 -0500

    Create post from arguments at posts/new

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,12 +1,12 @@
 import datetime
 
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for, jsonify
 from werkzeug import check_password_hash, generate_password_hash
 
 from app import db
 from app.posts.models import Post
 
-mod = Blueprint('posts', __name__, url_prefix='/posts')
+mod = Blueprint('posts', __name__, url_prefix='/posts/')
 
 
 @mod.route('')
@@ -14,3 +14,20 @@ def home():
     yesterday = datetime.datetime.utcnow() - datetime.timedelta(1)
     posts = Post.query.filter(Post.created_time > yesterday).order_by('created_time DESC').all()
     return render_template("posts/index.html", posts=posts)
+
+
+@mod.route('list')
+def list():
+    yesterday = datetime.datetime.utcnow() - datetime.timedelta(1)
+    posts = Post.query.filter(Post.created_time > yesterday).order_by('created_time DESC').all()
+    return jsonify(result=posts)
+
+
+@mod.route('new', methods=['POST'])
+def new():
+    post = Post(secret=request.args.get('secret'),
+            long=request.args.get('long'),
+            lat=request.args.get('lat'))
+    db.session.add(post)
+    return jsonify(post)
+
