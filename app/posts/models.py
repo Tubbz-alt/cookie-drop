376fda6def489df2eae477169d1d127cb106f837
Jason Myers <jason@jasonamyers.com>
2013-08-18 12:03:49 -0500

    Added Post.nearby_posts function
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,5 +1,14 @@
-from app import db
 import datetime
+from decimal import Decimal
+from math import pi, sqrt, radians, cos
+from geopy import distance, units
+
+from sqlalchemy.ext.hybrid import hybrid_method
+
+from app import db
+
+EARTH_RADIUS_KM = 6371.009
+KM_PER_DEG_LAT = 2 * pi * EARTH_RADIUS_KM / 360.0
 
 
 class Post(db.Model):
@@ -14,3 +23,25 @@ class Post(db.Model):
         self.secret = secret
         self.long = long
         self.lat = lat
+
+    @hybrid_method
+    def distance(self, lat, long):
+        km_per_deg_long = KM_PER_DEG_LAT * cos(radians(lat))
+        return sqrt((KM_PER_DEG_LAT * (lat - self.lat)) ** 2 + (km_per_deg_long * (long - self.long)))
+
+    @classmethod
+    def nearby_posts(cls, lat, long, dist):
+        nearby_posts = []
+        d = distance.distance(miles=dist)
+        rough_distance = Decimal(str(units.degrees(arcminutes=d.nm) * 2))
+        lat_lower = Decimal(lat) - rough_distance
+        lat_upper = Decimal(lat) + rough_distance
+        long_lower = Decimal(long) - rough_distance
+        long_upper = Decimal(long) + rough_distance
+        posts = db.session.query(Post).filter(Post.lat.between(lat_lower, lat_upper))
+        posts = posts.filter(Post.long.between(long_lower, long_upper))
+        for post in posts:
+            exact_distance = distance.distance((post.lat, post.long), (lat, long))
+            if exact_distance.miles <= dist:
+                nearby_posts.append(post)
+        return nearby_posts
