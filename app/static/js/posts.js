b0cf66d0492062bc2609844725f7d8d6c6993651
William Golden <egdelwonk@gmail.com>
2013-08-18 14:16:13 -0500

    Added clustered markers

--- app/static/js/posts.js
+++ app/static/js/posts.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
     if (Modernizr.geolocation) {
+    
     var map = L.mapbox.map('cookie-map', 'egdelwonk.map-e1ydkdp5', {
         attributionControl: false
     });
+
+    var postSource   = $("#post-template").html();
+    var posTemplate = Handlebars.compile(postSource);
+
+    var cookiePin = L.icon({
+        iconUrl: '/static/img/cookie-pin.png',
+        iconSize:     [23, 23], // size of the icon
+        iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [12, 5] // point from which the popup should open relative to the iconAnchor
+    });
+    
+    var markers = L.markerClusterGroup();
 
     navigator.geolocation.watchPosition(function(position) {
         $('div.nomap').hide();
@@ -21,43 +35,45 @@ $(document).ready(function() {
     
         map.fitBounds(radiusBounds.getBounds());
 
-        var userPin = L.icon({
-            iconUrl: '/static/img/user-pin.png',
-            iconSize:     [33, 62],
-            iconAnchor:   [16, 62]
-        });
-
-        var cookiePin = L.icon({
-            iconUrl: '/static/img/cookie-pin.png',
-            iconSize:     [23, 23], // size of the icon
-            iconAnchor:   [-10, 84], // point of the icon which will correspond to marker's location
-            shadowAnchor: [4, 62],  // the same for the shadow
-            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-        });
-
-        L.marker([position.coords.latitude, position.coords.longitude], {icon: userPin}).addTo(map);
-
-        var postSource   = $("#post-template").html();
-        var posTemplate = Handlebars.compile(postSource);
-        var posts = []
         $.get('/posts/list/' + position.coords.latitude + '/' + position.coords.longitude, function(data){
-            posts = data.result;
+            var posts = data.result;
             for (var i = 0; i < data.result.length; i++) {
-                L.marker([data.result[i].lat, data.result[i].long], {icon: cookiePin}).addTo(map);
+                var marker = L.marker([data.result[i].lat, data.result[i].long], {icon: cookiePin});
+                marker.bindPopup(data.result[i].secret);
+                markers.addLayer(marker);
             };
+            map.addLayer(markers);
             $('#posts tbody').html(posTemplate({results: posts}));
         });
         
     });
-
-
     } else {
         $('div.container div').hide();
         $('div#unsupported').show();
     }
-});
-$('.show-new-message-form').on('click', function(e) {
-    e.preventDefault();
-    $('#new-message-form').toggle();
-    $('#new-message-form input[name=secret]').focus();
+
+    $('.show-new-message-form').on('click', function(e) {
+        e.preventDefault();
+        $('#new-message-form').toggle();
+        $('#new-message-form input[name=secret]').focus();
+    });
+
+    $('#new-message-form form').on('submit', function(e){
+        e.preventDefault();
+        $.post($(this).attr('action'),$(this).serialize(), function(data){
+            var posts = data.result;
+            markers.clearLayers();
+            for (var i = 0; i < data.result.length; i++) {
+                var marker = L.marker([data.result[i].lat, data.result[i].long], {icon: cookiePin});
+                marker.bindPopup(data.result[i].secret);
+                markers.addLayer(marker);
+            };
+        
+            $('#posts tbody').html(posTemplate({results: posts}));
+
+            $('#new-message-form').toggle();
+            $('#new-message-form input[name=secret]').val('');
+        });
+    })
+
 });
