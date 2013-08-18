23f7dd1d465c35b3412a5d54df87c94e89f529dd
Jason Myers <jason@jasonamyers.com>
2013-08-18 10:37:55 -0500

    initial view
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

---
+++ app/posts/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+from werkzeug import check_password_hash, generate_password_hash
+
+from app import db
+from app.posts.models import Post
+
+mod = Blueprint('posts', __name__, url_prefix='/posts')
+
+
+@mod.route('/me/')
+def home():
+    yesterday = datetime.datetime.utcnow() - datetime.timedelta(1)
+    posts = Post.query.filter_by(Post.created_time > yesterday).order_by('created_time DESC').all()
+    return render_template("posts/index.html", posts=posts)
